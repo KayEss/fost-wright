1b4ad7cab643dd4e0e1c596ca67ecbce3afe4853
Kirit Sælensminde <k@kirit.com>
2016-11-12 10:42:53 +0700

    Initial project skeleton.

---
+++ Cpp/wright-exec-helper/wright-exec-helper.cpp
@@ -0,0 +1,23 @@
+/*
+    Copyright 2016, Felspar Co Ltd. http://support.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include <fost/cli>
+#include <fost/main>
+#include <fost/unicode>
+
+
+using namespace fostlib;
+
+
+FSL_MAIN(
+    L"wright-exec-helper",
+    L"Wright execution helper\nCopyright (c) 2016, Felspar Co. Ltd."
+)( fostlib::ostream &out, fostlib::arguments &args ) {
+    return 0;
+}
+
