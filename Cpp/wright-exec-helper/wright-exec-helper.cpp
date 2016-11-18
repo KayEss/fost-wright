9c80d2261a7dde582a1a29c4001bd3549b234595
Kirit Sælensminde <k@kirit.com>
2016-11-18 13:29:34 +0700

    Capture and display timing overhead between jobs in the worker.

--- Cpp/wright-exec-helper/wright-exec-helper.cpp
+++ Cpp/wright-exec-helper/wright-exec-helper.cpp
@@ -57,6 +57,10 @@ namespace {
         };
 
     void echo(std::istream &in, fostlib::ostream &out) {
+        decltype(std::chrono::high_resolution_clock::now()) from{}, to{};
+        const auto epoch = from.time_since_epoch();
+        decltype(from - to) total{};
+        unsigned int captures{};
         std::random_device rd;
         std::mt19937 gen(rd());
         std::normal_distribution<float> rand(1000, 500);
@@ -64,10 +68,18 @@ namespace {
         while ( in ) {
             std::getline(in, command);
             if ( in && not command.empty() ) {
+                to = std::chrono::high_resolution_clock::now();
+                if ( epoch < from.time_since_epoch() ) {
+                    total += (to - from);
+                    ++captures;
+                }
                 std::this_thread::sleep_for(rand(gen) * 1ms);
+                from = std::chrono::high_resolution_clock::now();
                 out << command << std::endl;
             }
         }
+        auto us = std::chrono::duration_cast<std::chrono::microseconds>(total).count();
+        std::cerr << (us / captures) << "us across " << captures << " loops" << std::endl;
     }
 }
 
