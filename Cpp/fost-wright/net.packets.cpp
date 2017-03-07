ba15d3debbd03264c6661a7645046fc58de789b5
Kirit Sælensminde <k@kirit.com>
2017-03-07 10:48:31 +0700

    Track the version packet properly.

--- Cpp/fost-wright/net.packets.cpp
+++ Cpp/fost-wright/net.packets.cpp
@@ -13,11 +13,17 @@
 #include <fost/rask/decoder-io.hpp>
 
 
+namespace {
+    fostlib::performance p_in_version(wright::c_exec_helper, "network", "in", "version");
+}
+
+
 void wright::in::version(std::shared_ptr<connection> cnx, rask::tcp_decoder &packet) {
+    ++p_in_version;
     const auto version = rask::read<uint8_t>(packet);
     const auto old_version = cnx->version(g_proto, version);
-    fostlib::log::error(c_exec_helper)
-        ("", "Version packet not processed")
+    fostlib::log::info(c_exec_helper)
+        ("", "Version packet processed")
         ("packet", "version", version)
         ("protocol", "version", g_proto.max_version())
         ("old", "version", old_version)
